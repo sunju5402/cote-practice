a2d76431ec8bfad7d1e1b891b6cba520c7a998d0
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2022-09-29 23:41:53 +0900

    Add files via upload

--- Test1.java
+++ Test1.java
@@ -1,25 +1,5 @@
-import java.util.Arrays;
-
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println(solution(15));
-    }
-    public static int solution(int n) {
-        int answer = 0;
-
-        boolean isPrime;
-        for (int i = 2; i < n; i++) {
-            isPrime = true;
-            for (int j = 2; j < i; j++) {
-                if (i % j == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            if (isPrime) {
-                answer++;
-            }
-        }
-        return answer;
+        System.out.println("Hello world!");
     }
 }
