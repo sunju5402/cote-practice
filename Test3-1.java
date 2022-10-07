dcf37eb63feb6d7b941fb70e9a201c218d97db9b
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2022-10-07 23:24:02 +0900

    Update Test3-1.java

--- Test3-1.java
+++ Test3-1.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+
+// 그래프 문제 (유향 그래프) 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(solution(3, new int[][]{{1, 3}, {2, 3}}));
+    }
+
+    public static int solution(int N, int[][] trust) {
+        int answer = -1;
+        int[] arr = new int[N + 1];
+
+        for (int i = 0; i < trust.length; i++) {
+            arr[trust[i][1]]++;
+            arr[trust[i][0]]--;
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == N - 1) {
+                return i;
+            }
+        }
+        return answer;
     }
 }
