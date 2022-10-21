ad9471fbd4b4152146d02c906e5d9ab4b003340f
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2022-10-21 23:39:26 +0900

    Update Test1.java

--- Cote5/Test1.java
+++ Cote5/Test1.java
@@ -1,1 +1,24 @@
+import java.util.Arrays;
 
+public class Test1 {
+    public static void main(String[] args) {
+        String s = "imfinethankyou";
+        String t = "atfhinemnoyuki";
+        System.out.println(solution(s, t));
+    }
+
+    public static boolean solution(String s, String t) {
+        if (s.length() != t.length()) { // 이거 안쓰면 효율성에서 6점 깍임.
+            return false;
+        }
+        char[] arr1 = s.toCharArray();
+        Arrays.sort(arr1);
+
+
+        char[] arr2 = t.toCharArray();
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+
+}
